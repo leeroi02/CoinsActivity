19d5c03365311a65d23318205f3a1be81b2b56c9
leeroi02 <felicityjcksn@gmail.com>
2024-11-26 15:40:41 +0800

    Add project files.

---
+++ CoinsActivity/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace CoinsActivity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numberOfCoins = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(781, 233);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Count Coins";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numberOfCoins
+            // 
+            this.numberOfCoins.Location = new System.Drawing.Point(629, 278);
+            this.numberOfCoins.Margin = new System.Windows.Forms.Padding(4);
+            this.numberOfCoins.Multiline = true;
+            this.numberOfCoins.Name = "numberOfCoins";
+            this.numberOfCoins.Size = new System.Drawing.Size(413, 124);
+            this.numberOfCoins.TabIndex = 1;
+            this.numberOfCoins.TextChanged += new System.EventHandler(this.numberOfCoins_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CoinsActivity.Properties.Resources.GetImage;
+            this.pictureBox1.Location = new System.Drawing.Point(107, 43);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(476, 570);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 822);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.numberOfCoins);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox numberOfCoins;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
